b15709d40fe657b149796900a4e6c61a3e660776
HanjaeHun <48702307+BankBoy22@users.noreply.github.com>
2023-09-20 21:32:32 +0900

    Update 수평,수직 슬라이드바 생성.c

--- week3/수평,수직 슬라이드바 생성.c
+++ week3/수평,수직 슬라이드바 생성.c
@@ -38,24 +38,24 @@ void draw_rectangle(int c, int r)
     unsigned char a=0xa6;
     unsigned char b[7]; 
     for(i=1;i<7;i++)
-   	b[i]=0xa0+i;
+        b[i]=0xa0+i;
 
     printf("%c%c",a, b[3]);
-    for(i=0;i<c;i++)
-  	printf("%c%c", a, b[1]);
+    for(i=0;i<c*2 + 2;i++) // 가로 길이를 1 더 크게 수정
+        printf("%c%c", a, b[1]);
     printf("%c%c", a, b[4]);
     printf("\n");
     for(i=0;i<r;i++)
     {
-	printf("%c%c", a, b[2]);
-	for(j=0;j<c;j++)
-		printf("  ");
-	printf("%c%c",a, b[2]);
-	printf("\n");
+        printf("%c%c", a, b[2]);
+        for(j=0;j<=c;j++)
+            printf("  ");
+        printf("%c%c",a, b[2]);
+        printf("\n");
     }
     printf("%c%c", a, b[6]);
-    for(i=0;i<c;i++)
-	printf("%c%c", a, b[1]);
+    for(i=0;i<c*2 + 2;i++) // 가로 길이를 1 더 크게 수정
+        printf("%c%c", a, b[1]);
     printf("%c%c", a, b[5]);
     printf("\n");
 }
@@ -112,3 +112,4 @@ void gotoxy(int x, int y)
    COORD Pos = {x - 1, y - 1};
    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
 }
+
